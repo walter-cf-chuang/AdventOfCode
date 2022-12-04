1dc445645a643bfd51b48b17a5b4806a8c8492d5
Walter Chuang <funboy820@gmail.com>
2022-12-04 11:02:22 +0800

    Initial Commit

---
+++ AdventOfCode/AdventOfCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCodeApp.swift
+//  AdventOfCode
+//
+//  Created by Walter on 2022/12/4.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AdventOfCode
+//
+//  Created by Walter on 2022/12/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
